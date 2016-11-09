1d99da2568c177d87125367fef1eb399cd48dc29
Aksel <aksel.christoffer.wiig@gmail.com>
2016-11-09 17:14:45 +0100

    la til leggtilØkt og layout fil til den

---
+++ app/src/main/java/com/example/aksel/s232324_mappe3/Leggtilokt.java
@@ -0,0 +1,16 @@
+package com.example.aksel.s232324_mappe3;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Aksel on 09/11/2016.
+ */
+
+public class Leggtilokt extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
