ff670cb0a0f6664ca8ab970fba11be420ffeaa48
Aksel <aksel.christoffer.wiig@gmail.com>
2016-11-16 16:50:57 +0100

    la til DBAdapter

---
+++ app/src/main/java/com/example/aksel/s232324_mappe3/DBAdapter.java
@@ -0,0 +1,70 @@
+package com.example.aksel.s232324_mappe3;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Aksel on 16/11/2016.
+ */
+
+public class DBAdapter {
+    static final String TAG="";
+    static final String DB_NAVN="Treningsdagbok";
+    static final String TABELL="Persliste";
+    static final String FORNAVN="fornavn";
+    static final String ETTERNAVN="etternavn";
+    static final String TDAG="dag", TMAANED="maaned", TAAR="aar";
+    static final String ID="id";
+    static final int DB_VERSJON=1;
+
+    private DatabaseHelper DBHelper;
+    private SQLiteDatabase db;
+    private Context context;
+
+    public DBAdapter(Context ctx){
+        this.context = ctx;
+        DBHelper = new DatabaseHelper(context);
+    }
+
+    private static class DatabaseHelper extends SQLiteOpenHelper{
+        DatabaseHelper(Context context){
+            super(context,DB_NAVN,null,DB_VERSJON);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            String sql="CREATE TABLE " + TABELL + "("
+                    + ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    + FORNAVN + " TEXT, "
+                    + ETTERNAVN + " TEXT, "
+                    + TDAG + " TEXT, "
+                    + TMAANED + " TEXT, "
+                    + TAAR + " TEXT)";
+            db.execSQL(sql);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            db.execSQL("drop table if exists " + TABELL);
+            onCreate(db);
+        }
+    }
+
+    public DBAdapter open() throws SQLException {
+        db = DBHelper.getWritableDatabase();
+        return this;
+    }
+
+    public void close(){}
+
+    public void insert(ContentValues cv){
+        db.insert(TABELL,null,cv);
+    }
+
+    public boolean slett(int id){
+        return db.delete(TABELL, ID + "='" + id + "'", null) > 0;
+    }
+}
