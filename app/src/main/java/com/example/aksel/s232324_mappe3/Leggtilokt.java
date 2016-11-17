2950f9d1cbe36698a2b0335de3b38464fd8d4f50
Aksel <aksel.christoffer.wiig@gmail.com>
2016-11-17 15:02:39 +0100

    la til datepicker, må fjerne år fra valghjulet

--- app/src/main/java/com/example/aksel/s232324_mappe3/Leggtilokt.java
+++ app/src/main/java/com/example/aksel/s232324_mappe3/Leggtilokt.java
@@ -3,15 +3,22 @@ package com.example.aksel.s232324_mappe3;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.DatePicker;
 
 /**
  * Created by Aksel on 09/11/2016.
  */
 
 public class Leggtilokt extends AppCompatActivity {
+
+    private DatePicker valg;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.leggtil_layout);
+
+        valg = (DatePicker) findViewById(R.id.datoValg);
+
     }
 }
