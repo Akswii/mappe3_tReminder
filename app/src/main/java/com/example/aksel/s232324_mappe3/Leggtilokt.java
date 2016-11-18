2d9e8929fc71908aa7ec8b26d6821b4ddf118cd5
Akswii <aksel.christoffer.wiig@gmail.com>
2016-11-18 13:13:46 +0100

    La til gridview, og prøver å fikse år i datepicker

--- app/src/main/java/com/example/aksel/s232324_mappe3/Leggtilokt.java
+++ app/src/main/java/com/example/aksel/s232324_mappe3/Leggtilokt.java
@@ -1,9 +1,14 @@
 package com.example.aksel.s232324_mappe3;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.DatePicker;
+
+import java.lang.reflect.Field;
 
 /**
  * Created by Aksel on 09/11/2016.
@@ -19,6 +24,25 @@ public class Leggtilokt extends AppCompatActivity {
         setContentView(R.layout.leggtil_layout);
 
         valg = (DatePicker) findViewById(R.id.datoValg);
-
+        try {
+            Field f[] = valg.getClass().getDeclaredFields();
+            for (Field field : f) {
+                if (field.getName().equals("mYearPicker")) {
+                    field.setAccessible(true);
+                    Object yearPicker = new Object();
+                    yearPicker = field.get(valg);
+                    ((View) yearPicker).setVisibility(View.GONE);
+                }
+            }
+        }
+        catch (SecurityException e) {
+            Log.d("ERROR", e.getMessage());
+        }
+        catch (IllegalArgumentException e) {
+            Log.d("ERROR", e.getMessage());
+        }
+        catch (IllegalAccessException e) {
+            Log.d("ERROR", e.getMessage());
+        }
     }
 }
