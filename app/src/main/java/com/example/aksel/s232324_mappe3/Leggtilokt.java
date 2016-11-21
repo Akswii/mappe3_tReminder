6b5d8fed34cf67f12df717236af0734bfef4317d
Aksel <aksel.christoffer.wiig@gmail.com>
2016-11-21 11:59:00 +0100

    la til kommentar

--- app/src/main/java/com/example/aksel/s232324_mappe3/Leggtilokt.java
+++ app/src/main/java/com/example/aksel/s232324_mappe3/Leggtilokt.java
@@ -43,6 +43,7 @@ public class Leggtilokt extends AppCompatActivity {
         }
         catch (IllegalAccessException e) {
             Log.d("ERROR", e.getMessage());
+            //ssssss
         }
     }
 }
