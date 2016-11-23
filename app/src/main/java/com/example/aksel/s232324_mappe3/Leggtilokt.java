64e395739524f41cede94aa29e96b6531f40b5b9
Aksel <aksel.christoffer.wiig@gmail.com>
2016-11-23 17:00:01 +0100

    div. forandringer ;)

--- app/src/main/java/com/example/aksel/s232324_mappe3/Leggtilokt.java
+++ app/src/main/java/com/example/aksel/s232324_mappe3/Leggtilokt.java
@@ -1,15 +1,10 @@
 package com.example.aksel.s232324_mappe3;
 
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
-import android.widget.DatePicker;
+import android.widget.Button;
 import android.widget.TimePicker;
-
-import java.lang.reflect.Field;
 import java.util.Calendar;
 
 /**
@@ -19,15 +14,19 @@ import java.util.Calendar;
 public class Leggtilokt extends AppCompatActivity {
 
     private TimePicker valg;
-    private Calendar klslett;
+    private Button lagre;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.leggtil_layout);
         getSupportActionBar().setTitle("");
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.mipmap.trening_ikon);
 
         valg = (TimePicker) findViewById(R.id.klokkeValg);
         valg.setIs24HourView(true);
+
+
     }
 }
