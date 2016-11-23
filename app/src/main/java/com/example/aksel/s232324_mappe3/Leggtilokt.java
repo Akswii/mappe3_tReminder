af656561f901adf0ad5179e27471451710655fc9
Aksel <aksel.christoffer.wiig@gmail.com>
2016-11-23 17:21:32 +0100

    lagt til div. knapper ol. i leggtilokt.java

--- app/src/main/java/com/example/aksel/s232324_mappe3/Leggtilokt.java
+++ app/src/main/java/com/example/aksel/s232324_mappe3/Leggtilokt.java
@@ -3,9 +3,13 @@ package com.example.aksel.s232324_mappe3;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TimePicker;
-import java.util.Calendar;
 
 /**
  * Created by Aksel on 09/11/2016.
@@ -14,7 +18,10 @@ import java.util.Calendar;
 public class Leggtilokt extends AppCompatActivity {
 
     private TimePicker valg;
+    private DatePicker dato;
+    private int dag, maaned;
     private Button lagre;
+    private EditText okt;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -26,7 +33,27 @@ public class Leggtilokt extends AppCompatActivity {
 
         valg = (TimePicker) findViewById(R.id.klokkeValg);
         valg.setIs24HourView(true);
+        okt = (EditText) findViewById(R.id.okt);
+        dato = (DatePicker) findViewById(R.id.datoValg);
+        lagre = (Button) findViewById(R.id.lagreokten);
+    }
 
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.leggtil_menu,menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.lagreokten:
+                String treningsokt = okt.getText().toString();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
 
     }
+
+
 }
