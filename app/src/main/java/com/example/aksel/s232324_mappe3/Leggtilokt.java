944421d586955238f55b0fdf9c515e0c3ccba5d8
Aksel <aksel.christoffer.wiig@gmail.com>
2016-11-23 16:39:28 +0100

    fikset dato til å være norsk tid

--- app/src/main/java/com/example/aksel/s232324_mappe3/Leggtilokt.java
+++ app/src/main/java/com/example/aksel/s232324_mappe3/Leggtilokt.java
@@ -7,8 +7,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.DatePicker;
+import android.widget.TimePicker;
 
 import java.lang.reflect.Field;
+import java.util.Calendar;
 
 /**
  * Created by Aksel on 09/11/2016.
@@ -16,34 +18,15 @@ import java.lang.reflect.Field;
 
 public class Leggtilokt extends AppCompatActivity {
 
-    private DatePicker valg;
+    private TimePicker valg;
+    private Calendar klslett;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.leggtil_layout);
 
-        valg = (DatePicker) findViewById(R.id.datoValg);
-        try {
-            Field f[] = valg.getClass().getDeclaredFields();
-            for (Field field : f) {
-                if (field.getName().equals("mYearPicker")) {
-                    field.setAccessible(true);
-                    Object yearPicker = new Object();
-                    yearPicker = field.get(valg);
-                    ((View) yearPicker).setVisibility(View.GONE);
-                }
-            }
-        }
-        catch (SecurityException e) {
-            Log.d("ERROR", e.getMessage());
-        }
-        catch (IllegalArgumentException e) {
-            Log.d("ERROR", e.getMessage());
-        }
-        catch (IllegalAccessException e) {
-            Log.d("ERROR", e.getMessage());
-            //ssssss
-        }
+        valg = (TimePicker) findViewById(R.id.klokkeValg);
+        valg.setIs24HourView(true);
     }
 }
